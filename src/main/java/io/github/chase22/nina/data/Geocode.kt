44a53cc64a3b8b6cfc2073efca079d706d84ad71
Chase <chase@mailbox.org>
2019-12-27 13:07:21 +0100

    Remove mapping classes
    they might be used again later, but aren't useful atm

--- src/main/java/io/github/chase22/nina/data/Geocode.kt
+++
@@ -1,8 +0,0 @@
-package io.github.chase22.nina.data
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Geocode(
-        @field:JsonProperty("valueName") val valueName: String,
-        @field:JsonProperty("value") val value: String
-)
--- src/main/java/io/github/chase22/nina/data/Value.kt
+++
@@ -1,8 +0,0 @@
-package io.github.chase22.nina.data
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Value(
-        @field:JsonProperty("valueName") val valueName: String,
-        @field:JsonProperty("value") val value: String
-)
