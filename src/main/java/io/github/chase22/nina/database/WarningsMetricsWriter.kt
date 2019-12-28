35c6d0ed057d9dfdc7b0ebce5d8f7df39a72e3e1
Chase <chase@mailbox.org>
2019-12-28 13:51:14 +0100

    Fix transaction problem in metrics writer

--- src/main/java/io/github/chase22/nina/database/WarningsMetricsWriter.kt
+++ src/main/java/io/github/chase22/nina/database/WarningsMetricsWriter.kt
@@ -3,19 +3,32 @@ package io.github.chase22.nina.database
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.MultiGauge
 import io.micrometer.core.instrument.Tags
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
 import java.util.function.Supplier
 
 class WarningsMetricsWriter(
         private val meterRegistry: MeterRegistry,
         private val warningsRepository: WarningsRepository
-        ) : Runnable {
+) : Runnable {
+
     override fun run() {
-        MultiGauge.builder("warnings.version.max").register(meterRegistry).register(getRows())
+        try {
+            MultiGauge.builder("warnings.version.max").register(meterRegistry).register(getRows())
+        } catch (e: Exception) {
+            LOGGER.error("Error updating Versions Gauge", e)
+        }
     }
 
     private fun getRows(): List<MultiGauge.Row<Supplier<Number>>> {
-        return warningsRepository.getIdentifiers().map {
-            MultiGauge.Row.of(Tags.of("identifier", it)) { warningsRepository.getCurrentVersion(it) }
+        return transaction {
+            warningsRepository.getIdentifiers().map {
+                MultiGauge.Row.of(Tags.of("identifier", it)) { transaction { warningsRepository.getCurrentVersion(it) } }
+            }
         }
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(WarningsMetricsWriter::class.java)
     }
 }
